8768c148cde9c819a49fb80a696a51edf4295427
Moishe Lettvin <moishel@gmail.com>
2022-01-05 10:22:48 -0700

    Some changes for #genuary output

--- sketch.js
+++ sketch.js
@@ -1,80 +1,227 @@
-function setup() {
-  createCanvas(1024, 1024);
-  for (i = 0; i < 10; i++) {
-    createActor()
+// Maximum number of actors. 
+// Can be arbitrarily big but perf will suffer above a few thousand
+let max_actors = 10000
+
+// Number of initial active actors. Anything up to max_actors
+let seed_actors = 400
+
+// Minimum lifespan for an actor. Anything between 2 (which would be silly) and whatever
+let min_lifespan = 1000
+
+// Additional random lifespan. Can be zero
+let additional_random_lifespan = 100
+
+// Chance of spawning if we find an inactive actor. Between 0 and 1.
+// Lower numbers may lead to colony collapse; must be balanced with
+// lifespan and number of actors
+let spawn_chance = 0.01
+
+// How much to randomize a spawned actor's direction, relative to its parent.
+let spawn_direction_randomization = 0.3
+
+// How much an actor should just wander around
+let random_wander = 0.1
+
+// How narrow is an actor's view of the world (bigger is narrower)
+let narrowness = 6
+
+// How far ahead does an actor look
+let look_distance = 10
+
+// How quickly does the board fade. Between 0 and 255
+let fade_speed = 8
+
+// How intense is an actor's color blended. Between 0 and 255
+let actor_blend_amount = 196
+
+// Can set this to nil and will default to random placement. The
+// function specified here will define variables for arranging
+// the initial layout (margins, spacing, etc).
+let arrangement = rectangleActor
+
+let save_animation = true
+let animation_frames = 240
+let save_interval = 1
+
+function getLifespan() {
+  return min_lifespan + Math.floor(Math.random() * additional_random_lifespan);
+}
+
+function rectangleActor(idx) {
+  let margin = 0.3
+
+  var a = randomActor(idx)
+  var actors_per_side = sqrt(seed_actors)
+  var x = idx % actors_per_side
+  var y = Math.floor(idx / actors_per_side)
+
+  var offset_x = width * margin
+  var offset_y = height * margin
+
+  var spacing_x = ((1 - margin * 2) * width) / actors_per_side
+  var spacing_y = ((1 - margin * 2) * height) / actors_per_side
+
+  a.x = offset_x + x * spacing_x
+  a.y = offset_y + y * spacing_y
+
+  a.color = {
+    r: 255,
+    g: 255,
+    b: 255
   }
 
+  return a
+}
+
+let c
+let capturer
+
+function setup() {
+  c = createCanvas(256, 256); //windowWidth, windowHeight);
+  for (i = 0; i < max_actors; i++) {
+    createActor(i, arrangement)
+  }
   background(0)
+
+  if (save_animation) {
+    capturer = new CCapture( { format: 'png' } );
+  }
 }
 
 var actors = []
 
-function createActor() {
-  var colors = [
-    {r: 255, g: 0, b: 0},
-    {r: 0, g: 255, b: 0},
-    {r: 0, g: 0, b: 255}
-  ]
+function randomActor(idx) {
+  var lifespan
+  if (idx < seed_actors) {
+    lifespan = getLifespan()
+  } else {
+    lifespan = 0
+  }    
+  
   var a = {
     x: Math.floor(Math.random() * width),
     y: Math.floor(Math.random() * height),
     direction: Math.random() * Math.PI * 2,
-    color: colors[Math.floor(Math.random() * colors.length)]
+    lifespan: lifespan
   }
+  a.color = {
+    r: Math.floor(Math.random() * 255),
+    g: Math.floor(Math.random() * 255),
+    b: Math.floor(Math.random() * 255),
+  }
+  return a
+}
+
+function createActor(idx, actorFromIdx=randomActor) {
+  var a = actorFromIdx(idx)
   actors.push(a)
 }
 
 function spawnActor() {
   seed = actors[Math.floor(Math.random() * actors.length)]
+  if (seed.lifespan <= 1) {
+    return false
+  }
+
   return {
     x: seed.x,
     y: seed.y,
-    direction: seed.direction + Math.random() - 0.5,
+    direction: seed.direction + (Math.random() - 0.5) * spawn_direction_randomization,
     color: seed.color
   }
 }
 
+let frame = 0
+
 function generation() {
-  actors.forEach(actor => {
-    actor.x += cos(actor.direction)
-    actor.y += sin(actor.direction)
+  if (frame == 0 && save_animation) {
+    capturer.start()      
+  }
 
-    if (actor.x > width || actor.y > height || actor.x < 0 || actor.y < 0) {
-      new_actor = spawnActor()
-      actor.x = new_actor.x
-      actor.y = new_actor.y
-      actor.direction = new_actor.direction
-      actor.color = new_actor.color
+  for (var i = 0; i < actors.length; i++) {
+    if (actors[i].lifespan <= 1) {
+      if (Math.random() < spawn_chance) {
+        new_actor = spawnActor()
+        if (new_actor) {
+          actors[i].x = new_actor.x
+          actors[i].y = new_actor.y
+          actors[i].direction = new_actor.direction
+          actors[i].color = new_actor.color
+          actors[i].lifespan = getLifespan()  
+        }
+      }
+    } else {
+      if (actors[i].lifespan > 1) {
+        actors[i].x += cos(actors[i].direction)
+        actors[i].y += sin(actors[i].direction)
+    
+        if (actors[i].x > width || actors[i].y > height || actors[i].x < 0 || actors[i].y < 0) {
+          new_actor = spawnActor()
+          if (!new_actor) {
+            continue;
+          }
+          actors[i].x = new_actor.x
+          actors[i].y = new_actor.y
+          actors[i].direction = new_actor.direction
+          actors[i].color = new_actor.color
+          actors[i].lifespan = getLifespan()
+        }
+    
+        actors[i].lifespan -= 1
+        actors[i].direction += (Math.random() - 0.5) * random_wander
+    
+        // look around
+        var dir = actors[i].direction
+        var my_color = createVector(actors[i].color.r, actors[i].color.g, actors[i].color.b).normalize()
+        var max_value = 0
+        for (var j = 0; j < 3; j++) {
+          var look_direction = actors[i].direction - Math.PI / narrowness + (Math.PI / narrowness) * j
+          look_at_x = actors[i].x + cos(look_direction) * look_distance
+          look_at_y = actors[i].y + sin(look_direction) * look_distance
+    
+          var c = get(look_at_x, look_at_y)
+          var color_at = createVector(c[0], c[1], c[2]).normalize()
+          var dx = color_at.dot(my_color)
+    
+          if (dx > max_value) {
+            max_value = dx
+            dir = look_direction
+          }
+        }
+    
+        actors[i].direction = dir
+      }
     }
+  }
 
-    actor.direction += (Math.random() - 0.5) * 0.1
-  })
+  if (frame == animation_frames && save_animation) {
+    noLoop()
+    capturer.stop()
+    capturer.save()
+  }
+
+  if (save_animation && (frame % save_interval == 0)) {
+    capturer.capture(document.getElementById('defaultCanvas0'));    
+  }
+  
+  frame += 1
 }
 
-let interval = 20
-
 function draw() {
-  //background(0)
-  /*
-  for (var i = 0; i < height; i += (interval + 1)) {
-    stroke(i / height * 255, i / height * 128, i / height * 255)
-    line(i, 0, 0, height - i)
-    stroke(255 - i / height * 255, i / height * 128, i / height * 255)
-    line(i, width, height, height - i)
-    line(width - i, 0, width, height - i)
-    line(i, height, 0, i)
-  }
-  */
+  fill(0, 0, 0, fade_speed)
+  rect(0, 0, width, height)
 
   generation()
-  strokeWeight(10)
+  //strokeWeight(5)
   prev_x = actors[0].x
   prev_y = actors[0].y
   actors.forEach(actor => {
-    stroke(actor.color.r, actor.color.g, actor.color.b, 32)
-    point(actor.x, actor.y)
-    //line(prev_x, prev_y, actor.x, actor.y)
-    prev_x = actor.x
-    prev_y = actor.y
+    if (actor.lifespan > 1) {
+      stroke(actor.color.r, actor.color.g, actor.color.b, actor_blend_amount)
+      point(actor.x, actor.y)
+      //line(prev_x, prev_y, actor.x, actor.y)
+      prev_x = actor.x
+      prev_y = actor.y  
+    }
   })
 }
