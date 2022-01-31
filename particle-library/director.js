5912a75ef01534bbb538ea814fec06ab7395513b
Moishe Lettvin <moishel@gmail.com>
2022-01-31 13:45:13 -0700

    Refactor; extend Director to change behavior

--- particle-library/director.js
+++ particle-library/director.js
@@ -1,133 +1,123 @@
 let Director = class {
-    max_actors
-    seed_actors
-    max_age
-    min_age
-    create_func
-    update_func
-    should_spawn_func
-    draw_func
+  max_actors
+  seed_actors
+  max_age
+  min_age
 
-    actors = []
-    next_free = -1
+  actors = []
+  next_free = -1
 
-    static default_create_func(min_age, max_age, parent=null) {
-      if (parent) {
-        let actor = Object.assign({}, parent)
-        return actor
-      } else {
-        let actor = {
-          x: width / 2,
-          y: height / 2,
-          d: 0,
-          v: 0,
-          lifetime: Math.floor(Math.random() * max_age) + min_age,
-          age: 0,
-          is_active: true,
-          generation_created: 0,
-          next_free: -1
+
+  constructor(max_actors,
+    seed_actors,
+    max_age,
+    min_age) {
+    this.max_actors = max_actors
+    this.seed_actors = seed_actors
+    this.max_age = max_age
+    this.min_age = min_age
+  }
+
+  initialize() {
+    for (let i = 0; i < this.seed_actors; i++) {
+      this.actors[i] = this.create()
+    }
+
+    for (let i = this.seed_actors; i < this.max_actors; i++) {
+      this.actors[i] = {
+        next_free: i == this.seed_actors ? -1 : i - 1,
+        is_active: false
+      }
+    }
+    this.next_free = this.max_actors - 1
+  }
+
+  process_and_draw(generation) {
+    for (let i = 0; i < this.actors.length; i++) {
+      if (!this.actors[i].is_active || this.actors[i].generation_created == generation) {
+        continue
+      }
+
+      this.draw(this.actors[i])
+
+      if (!this.update(this.actors[i])) {
+        this.delete_actor(i)
+      } else if (this.should_spawn(this.actors[i])) {
+        let actor = this.spawn(this.actors[i])
+        if (actor) {
+          actor.generation_created = generation
         }
-        return actor
-      }
-    }
-
-    static default_update_func(actor) {
-      actor.x += cos(actor.d) * actor.v
-      actor.y += sin(actor.d) * actor.v
-
-      actor.age += 1
-      return actor.age < actor.lifetime &&
-        actor.x >= 0 &&
-        actor.y >= 0 &&
-        actor.x < width &&
-        actor.y < height
-    }
-
-    static default_draw_func(actor) {
-      stroke(0, 0, 0, 255 - Math.floor(256 * (actor.age / actor.lifetime)))
-      strokeWeight(0.5)
-      point(actor.x, actor.y)
-
-    }
-
-    static default_should_spawn_func(actor) {
-      return Math.random() < 0.1
-    }
-
-    constructor(max_actors,
-                seed_actors,
-                max_age,
-                min_age,
-                create_func=Director.default_create_func,
-                update_func=Director.default_update_func,
-                should_spawn_func=Director.default_should_spawn_func,
-                draw_func=Director.default_draw_func) {
-      this.max_actors = max_actors
-      this.seed_actors = seed_actors
-      this.max_age = max_age
-      this.min_age = min_age
-      this.create_func = create_func
-      this.update_func = update_func
-      this.should_spawn_func = should_spawn_func
-      this.draw_func = draw_func
-    }
-
-    initialize() {
-      for (let i = 0; i < this.seed_actors; i++) {
-        this.actors[i] = this.create_func(this.min_age, this.max_age)
-      }
-
-      for (let i = this.seed_actors; i < this.max_actors; i++) {
-        this.actors[i] = {
-          next_free: i == this.seed_actors ? -1 : i - 1,
-          is_active: false
-        }
-      }
-      this.next_free = this.max_actors - 1
-    }
-
-    process_and_draw(generation) {
-      for (let i = 0; i < this.actors.length; i++) {
-        if (!this.actors[i].is_active || this.actors[i].generation_created == generation) {
-          continue
-        }
-
-        this.draw_func(this.actors[i])
-
-        if (!this.update_func(this.actors[i])) {
-          this.delete_actor(i)
-        } else if (this.should_spawn_func(this.actors[i])) {
-          actor = this.spawn_actor(this.actors[i])
-          if (actor) {
-            actor.generation_created = generation
-          }
-        }
-      }
-    }
-
-    delete_actor(index) {
-      this.actors[index] = {
-        is_active: false,
-        next_free: this.next_free
-      }
-      this.next_free = index
-    }
-
-    spawn_actor(actor) {
-      if (this.next_free != -1) {
-        let idx = this.next_free
-        this.next_free = this.actors[idx].next_free
-
-        this.actors[idx] = this.create_func(this.min_age, this.max_age, actor)
-        this.actors[idx].x = actor.x
-        this.actors[idx].y = actor.y
-        this.actors[idx].lifetime = this.min_age + Math.floor(Math.random() * this.max_age)
-        this.actors[idx].age = 0
-        this.actors[idx].v = 0
-        this.actors[idx].d = actor.d + (Math.random() - 0.5) * Math.PI / 8
-        this.actors[idx].next_free = -1
-
-        return this.actors[idx]
       }
     }
   }
+
+  delete_actor(index) {
+    this.actors[index] = {
+      is_active: false,
+      next_free: this.next_free
+    }
+    this.next_free = index
+  }
+
+  create(parent = null) {
+    if (parent) {
+      let actor = Object.assign({}, parent)
+      return actor
+    } else {
+      let actor = {
+        x: width / 2,
+        y: height / 2,
+        d: 0,
+        v: 0,
+        lifetime: Math.floor(Math.random() * this.max_age) + this.min_age,
+        age: 0,
+        is_active: true,
+        generation_created: 0,
+        next_free: -1
+      }
+      return actor
+    }
+  }
+
+  update(actor) {
+    actor.x += cos(actor.d) * actor.v
+    actor.y += sin(actor.d) * actor.v
+
+    actor.age += 1
+    return actor.age < actor.lifetime &&
+      actor.x >= 0 &&
+      actor.y >= 0 &&
+      actor.x < width &&
+      actor.y < height
+  }
+
+  spawn(actor) {
+    if (this.next_free != -1) {
+      let idx = this.next_free
+      this.next_free = this.actors[idx].next_free
+
+      this.actors[idx] = this.create(actor)
+      this.actors[idx].x = actor.x
+      this.actors[idx].y = actor.y
+      this.actors[idx].lifetime = this.min_age + Math.floor(Math.random() * this.max_age)
+      this.actors[idx].age = 0
+      this.actors[idx].v = 0
+      this.actors[idx].d = actor.d + (Math.random() - 0.5) * Math.PI / 2
+      this.actors[idx].next_free = -1
+
+      return this.actors[idx]
+    }
+
+    return null
+  }
+
+  draw(actor) {
+    stroke(0, 0, 0, 255 - Math.floor(256 * (actor.age / actor.lifetime)))
+    strokeWeight(0.5)
+    point(actor.x, actor.y)
+  }
+
+  should_spawn(actor) {
+    return Math.random() < 0.1
+  }
+}
--- particle-library/sketch.js
+++ particle-library/sketch.js
@@ -3,48 +3,70 @@ let capturer
 let save_animation = true
 let max_frames = 60 * 10
 
+class MoldDirector extends Director {
+  create(parent) {
+    let actor = super.create(parent)
+
+    actor.x = Math.random() * width
+    actor.y = Math.random() * height
+    actor.d = Math.random() * PI * 2
+    return actor
+  }
+
+  update(actor) {
+    let result = super.update(actor)
+    if (result) {
+      actor.v = min(1, actor.v + 0.1)
+
+      let dirs = [-Math.PI / 4, 0, Math.PI / 4]
+      let big_c = 255
+      let dir = actor.d
+      for (let i = 0; i < dirs.length; i++) {
+        let look_x = actor.x + cos(dirs[i] + actor.d) * 3
+        let look_y = actor.y + sin(dirs[i] + actor.d) * 3
+        let c = get(look_x, look_y)[0]
+        if (c < big_c) {
+          c = big_c
+          dir = dirs[i] + actor.d
+        }
+      }
+
+      actor.d = lerp(actor.d, dir, Math.random())
+      actor.d += (Math.random() - 0.5) * 0.1
+    }
+    return result
+  }
+
+  should_spawn(actor) {
+    let c = get(actor.x, actor.y)[0]
+    return Math.random() * 255 < c
+    //return Math.random() < actor.age / actor.lifetime
+  }
+
+  draw(actor) {
+    stroke(0, 0, 0, 255 - Math.floor(255 * (actor.age / actor.lifetime)))
+    strokeWeight(0.3)
+    point(actor.x, actor.y)
+  }
+
+  spawn(actor) {
+    let new_actor = super.spawn(actor)
+    if (new_actor) {
+      new_actor.d = actor.d
+    }
+    return new_actor
+}
+}
+
 function setup() {
   //createCanvas(windowWidth, windowHeight);
   createCanvas(1024, 1024)
   background(255)
-  director = new Director(
+  director = new MoldDirector(
     5000, // max actors
-    1,  // seed actors
+    7,  // seed actors
     1000,  // max age
-    100,   // min_age
-    (min_age, max_age, parent) => {
-      actor = Director.default_create_func(min_age, max_age, parent)
-      actor.x = Math.random() * width
-      actor.y = Math.random() * height
-      actor.d = Math.random() * PI * 2
-      return actor
-    },
-    (actor) => {
-      let result = Director.default_update_func(actor)
-      if (result) {
-        actor.v = min(1, actor.v + 0.1)
-
-        let dirs = [-Math.PI / 4, 0, Math.PI / 4]
-        let big_c = 255
-        let dir = actor.d
-        for (let i = 0; i < dirs.length; i++) {
-          let look_x = actor.x + cos(dirs[i] + actor.d) * 3
-          let look_y = actor.y + sin(dirs[i] + actor.d) * 3
-          let c = get(look_x, look_y)[0]
-          if (c < big_c) {
-            c = big_c
-            dir = dirs[i] + actor.d
-          }
-        }
-
-        actor.d = lerp(actor.d, dir, Math.random())
-        actor.d += (Math.random() - 0.5) * 0.1
-      }
-      return result
-    },
-    (actor) => {
-      return Math.random() < actor.age / actor.lifetime
-    }
+    100
   )
   director.initialize()
 
