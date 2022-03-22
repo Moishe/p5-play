93dc97426d19018dac125e83da3c3890e7d381bd
Moishe Lettvin <moishel@gmail.com>
2022-03-22 07:52:51 -0600

    Improved drawing, also added SVG output

--- circlefrac/sketch.js
+++ circlefrac/sketch.js
@@ -1,19 +1,60 @@
 let radii = []
-let recursion_depth = 8
+let recursion_depth = 11
 let point_count = 2 ** recursion_depth
 var radius = 256
 
 let BORDER = 20
-let LAYERS = 1
-let MAX_DEPTH = 8
+let LAYERS = 100
+let MAX_DEPTH = 11
 let RANDOM_RADIUS_DIVISOR = 0.1
-let SHOULD_LOOP = true
+let SHOULD_LOOP = false
+let SHOULD_EXPAND = true
 
 let DEBUG_OUTPUT = false
 
+let SVG_OUTPUT = true
+
+let SVG_MM = 3.543307
+
+let SVG_WIDTH = 210
+let SVG_HEIGHT = 290
+
+let SVG_HEADER = `<svg width="${SVG_WIDTH}mm" height="${SVG_HEIGHT}mm" xmlns="http://www.w3.org/2000/svg">\n`
+let SVG_FOOTER = '</svg>\n'
+
+let SVG_OUTPUT_STRING = ''
+
+let CURRENT_SVG_PATH = ''
+
+function beginSvg() {
+  SVG_OUTPUT_STRING = SVG_HEADER
+}
+
+function beginSvgPath() {
+  CURRENT_SVG_PATH = ''
+}
+
+function svgMove(x, y) {
+  CURRENT_SVG_PATH += ` M ${x} ${y}`
+}
+
+function svgLine(x, y) {
+  CURRENT_SVG_PATH += ` L ${x} ${y}`
+}
+
+function endSvgPath() {
+  SVG_OUTPUT_STRING += `<path d="${CURRENT_SVG_PATH}" stroke="black" fill-opacity="0" stroke-width="0.2"/>\n`
+  CURRENT_SVG_PATH = ''
+}
+
+function endSvg() {
+  SVG_OUTPUT_STRING += SVG_FOOTER
+  save([SVG_OUTPUT_STRING], 'p5-svg.svg')
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  radius = (Math.min(windowWidth, windowHeight) - BORDER * 2) / 3
+  radius = (Math.min(windowWidth, windowHeight) - BORDER * 2) / 2
   for (let i = 0; i < point_count; i++) {
     radii[i] = 1 // radius * (i / point_count)
   }
@@ -51,7 +92,7 @@ function interpolateNewRadius(start, count) {
 function getPoint(i, multiplier=1) {
   let t = i / point_count * Math.PI * 2 + Math.PI / 2
   let r = radii[i]
-  let offset = radius * 3 / 2 + BORDER
+  let offset = radius + BORDER
   let x = cos(t) * r * multiplier + offset
   let y = sin(t) * r * multiplier + offset
 
@@ -109,6 +150,7 @@ function breakCircle(start, count, depth=0) {
 
 let start_point = 0
 function draw() {
+  beginSvg()
   fill(255, 255, 255, 32)
   rect(0, 0, windowWidth, windowHeight)
 
@@ -121,15 +163,26 @@ function draw() {
     stroke(0)
     strokeWeight(0.1)
     beginShape()
+    beginSvgPath()
     for (let i = 0; i < point_count; i++) {
-      let xy = getPoint(i, radius / max_r)
-      //let xy = getPoint(i, (m + 1) / LAYERS * radius / max_r)
+      var factor = 1
+      if (SHOULD_EXPAND) {
+        factor = (m + 1) / LAYERS
+      }
+      let xy = getPoint(i, factor * radius / max_r)
       let x = xy[0]
       let y = xy[1]
+      if (i == 0) {
+        svgMove(x, y)
+      } else {
+        svgLine(x, y)
+      }
       vertex(x,y)
     }
+    endSvgPath()
     endShape(CLOSE)
   }
   if (!SHOULD_LOOP)
     noLoop()
+  endSvg()
 }
